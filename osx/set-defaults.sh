661ec1a0dd4825d4f0f00c91406c0e565b97eacd
mcsnootch <mcsnootch@gmail.com>
2014-06-11 20:48:14 +0100

    Set default folder mode to show hidden, and attempted to fix rvm path problems

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -41,3 +41,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Show hidden files in finder
+defaults write com.apple.finder AppleShowAllFiles TRUE
